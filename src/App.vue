cd2c1a024af2e331e0df3d0d9a322422c57a2c45
HanxgSir <18304018283@163.com>
2019-05-09 09:43:49 +0800

    添加图片

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
-
+<style>
+  #app {
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 80px;
+  }
+</style>
 <script>
   export default {
     name: 'App',
